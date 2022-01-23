d5972e55d24b6c0da1514020bc89550d0b353919
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-01-23 10:06:57 +0000

    cookies

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express();
 
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ];
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -21,10 +15,14 @@ app.get('/cards', (req, res) => {
     res.render('card', {prompt: 'Who is buried in Grants tomb?', hint: 'Guess what'})
 })
 
-app.get('/sandbox', (req, res) => {
-    res.render('sandbox', {colors: colors})
+app.get('/hello', (req, res) => {
+    res.render('hello')
 })
 
+app.post('/hello', (req,res) => {
+    res.cookie('username', req.body.username)
+    res.render('hello', {name:req.body.username})
+})
 app.listen(3000, () => {
     console.log('The app is running at LocalHost:3000')
 })
