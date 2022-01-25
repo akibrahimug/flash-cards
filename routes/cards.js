083b389f7920b941b33d126ab35625f1098dcadc
Kasoma Ibrahim <kasomaibrahim@gmail.com>
2022-01-25 13:09:53 +0000

    Navigation

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const {data} = require('../data/flashcarddata.json');
+const {cards} = data;
 
-router.get('/', (req, res) => {
-    res.render('card', {prompt: 'Who is buried in Grants tomb?', hint: 'Guess what'})
+router.get('/', (req,res) => {
+    const randomNum = Math.floor(Math.random()*cards.length);
+    res.redirect(`/cards/${randomNum}?side=question`)
 })
+
+router.get('/:id', (req, res) => {
+    const {side} = req.query;
+    const {id} = req.params;
+    if(!side){
+        res.redirect(`/cards/${id}?side=question`)
+    }
+    const text = cards[id][side];
+    const {hint} = cards[id];
+
+    const templateData = {id, text};
+    if(side === 'question'){
+        templateData.hint = hint;
+        templateData.sideToShow = 'answer';
+        templateData.linkText = 'Answer'
+    }else if(side === 'answer'){
+        templateData.sideToShow =  'question';
+        templateData.linkText = 'Question'
+       
+    }
+    res.render('card', templateData)
+})
+
 
 module.exports = router;
